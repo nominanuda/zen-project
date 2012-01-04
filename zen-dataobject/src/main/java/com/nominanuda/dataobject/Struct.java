408fac476946ae46f27eb786738d09b2b63a322a
michele <michelegonella@gmail.com>
2012-01-04 18:07:25 +0100

    experimental

---
+++ zen-dataobject/src/main/java/com/nominanuda/dataobject/Struct.java
@@ -0,0 +1,9 @@
+package com.nominanuda.dataobject;
+
+
+public @interface Struct {
+	boolean listing() default false;
+	String listingPath() default "results";
+	Class<?> cls() default DataStruct.class;
+
+}
