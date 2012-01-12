cf344d7630fa73452d88eba6ada592063e57f1d6
michele <michelegonella@gmail.com>
2012-01-12 13:04:10 +0100

    added listing constant

--- zen-dataobject/src/main/java/com/nominanuda/dataobject/Struct.java
+++ zen-dataobject/src/main/java/com/nominanuda/dataobject/Struct.java
@@ -5,5 +5,7 @@ public @interface Struct {
 	boolean listing() default false;
 	String listingPath() default "results";
 	Class<?> cls() default DataStruct.class;
+	
+	String results = "results";
 
 }
