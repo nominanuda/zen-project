a8763f69dd03baf228d95872d628e3ad93bf2654
michele <michelegonella@gmail.com>
2012-01-12 14:09:09 +0100

    cls must be specified

--- zen-dataobject/src/main/java/com/nominanuda/dataobject/Struct.java
+++ zen-dataobject/src/main/java/com/nominanuda/dataobject/Struct.java
@@ -3,8 +3,8 @@ package com.nominanuda.dataobject;
 
 public @interface Struct {
 	boolean listing() default false;
-	String listingPath() default "results";
-	Class<?> cls() default DataStruct.class;
+	String listingPath() default results;
+	Class<? extends DataObject> cls();
 	
 	String results = "results";
 
