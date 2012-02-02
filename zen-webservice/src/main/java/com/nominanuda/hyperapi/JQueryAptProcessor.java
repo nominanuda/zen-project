a4845e4c904928d939eac459c34498f38c954150
michele <michelegonella@gmail.com>
2012-02-02 17:04:30 +0100

    initial impl, needs refactoring and testing

--- zen-webservice/src/main/java/com/nominanuda/hyperapi/JQueryAptProcessor.java
+++ zen-webservice/src/main/java/com/nominanuda/hyperapi/JQueryAptProcessor.java
@@ -2,6 +2,8 @@ package com.nominanuda.hyperapi;
 
 import java.io.IOException;
 import java.io.Writer;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Set;
 
 import javax.annotation.processing.Completion;
@@ -14,18 +16,52 @@ import javax.lang.model.element.Element;
 import javax.lang.model.element.ElementKind;
 import javax.lang.model.element.ExecutableElement;
 import javax.lang.model.element.TypeElement;
+import javax.lang.model.element.VariableElement;
+import javax.lang.model.type.DeclaredType;
+import javax.lang.model.type.PrimitiveType;
+import javax.lang.model.type.TypeMirror;
 import javax.tools.JavaCompiler;
 import javax.tools.JavaFileManager.Location;
 import javax.tools.FileObject;
 import javax.tools.StandardJavaFileManager;
 import javax.tools.StandardLocation;
 import javax.tools.ToolProvider;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.QueryParam;
 
+import com.nominanuda.lang.Check;
 import com.nominanuda.lang.Collections;
+import com.nominanuda.lang.Strings;
 
 public class JQueryAptProcessor implements Processor {
 	private ProcessingEnvironment env;
-	String prologue = "(function() {var dummy = function(){};var uritpl = function(spec, model) {var pathAndQuery = spec.split('?');var re = /\\{[^\\}]+\\}/g;var result = pathAndQuery[0].replace(re,function(str, p1, p2, offset, s) {return model[str.replace(/\\{(\\w+).*/, '$1')];});if(pathAndQuery.length > 1) {result += '?' + pathAndQuery[1].replace(re,function(str, offset) {var x = str.replace(/\\{(\\w+).*/, '$1');var p = model[x];var isPname = offset === 0 || pathAndQuery[1].charAt(offset - 1) ==! '=';return ''+ (isPname ? x+'='+p : p);});};return result;};";
+	String prologue = //"(function() {var dummy = function(){};var uritpl = function(spec, model) {var pathAndQuery = spec.split('?');var re = /\\{[^\\}]+\\}/g;var result = pathAndQuery[0].replace(re,function(str, p1, p2, offset, s) {return model[str.replace(/\\{(\\w+).*/, '$1')];});if(pathAndQuery.length > 1) {result += '?' + pathAndQuery[1].replace(re,function(str, offset) {var x = str.replace(/\\{(\\w+).*/, '$1');var p = model[x];var isPname = offset === 0 || pathAndQuery[1].charAt(offset - 1) ==! '=';return ''+ (isPname ? x+'='+p : p);});};return result;};";
+"(function() {\n"+
+"var dummy = function(){};\n"+
+"var uritpl = function(spec, model) {\n"+
+"  var pathAndQuery = spec.split('?');\n"+
+"  var re = /\\{[^\\}]+\\}/g;\n"+
+"  var result = pathAndQuery[0].replace(re,function(str, p1, p2, offset, s) {\n"+
+"    return model[str.replace(/\\{(\\w+).*/, '$1')];\n"+
+"  });\n"+
+"  if(pathAndQuery.length > 1) {\n"+
+"    result += '?' + pathAndQuery[1].replace(re,function(str, offset) {\n"+
+"      var x = str.replace(/\\{(\\w+).*/, '$1');\n"+
+"      var p = model[x];\n"+
+"      var isPname = offset === 0 || pathAndQuery[1].charAt(offset - 1) ==! '=';\n"+
+"      return ''+ (isPname ? x+'='+p : p);\n"+
+"    });\n"+
+"  }\n"+
+"  return result;\n"+
+"};\n";
+
+	
+	
 	String epilogue = "})();";
 
 	public boolean process(Set<? extends TypeElement> annotations,
@@ -54,25 +90,88 @@ public class JQueryAptProcessor implements Processor {
 //  },
 //}
 	private void processApi(TypeElement el, RoundEnvironment roundEnv) {
-		StringBuilder sb = new StringBuilder();
 		try {
 			String targetType = el.getQualifiedName().toString();
 			log("targetType:"+targetType);
 			String pkg = targetType.substring(0, targetType.lastIndexOf('.'));
+			String ns = "hyperapi_"+targetType.replace('.', '_');
 			log("detected package:"+pkg);
-			FileObject jsSrc = env.getFiler().createResource(StandardLocation.CLASS_OUTPUT, pkg, "foobar", el);
+			FileObject jsSrc = env.getFiler().createResource(StandardLocation.CLASS_OUTPUT, pkg, ns+".js", el);
 			Writer w = jsSrc.openWriter();
-			w.write("la vispa teresa");
+			w.write(prologue);
+			w.write("window."+ns+" = {");
 			for(Element member : el.getEnclosedElements()) {
 				if(member.getKind() == ElementKind.METHOD) {
 					log(member.getSimpleName().toString());
+					writeMethodCall((ExecutableElement)member, w, ns);
 				}
 			}
+			w.write("};\n");
+			w.write(epilogue);
 			w.close();
+			log("generating:"+jsSrc.toUri().toString());
+			
 		} catch (IOException e) {
 			throw new RuntimeException(e);
 		}
 	}
+
+	private void writeMethodCall(ExecutableElement member, Writer w, String ns) throws IOException {
+		List<? extends VariableElement> params = member.getParameters();
+		Path uriTpl = Check.notNull(member.getAnnotation(Path.class));
+		String method = member.getAnnotation(GET.class) != null
+			? "GET" : member.getAnnotation(POST.class) != null
+			? "POST" : member.getAnnotation(PUT.class) != null
+			? "PUT" : member.getAnnotation(DELETE.class) != null
+			? "DELETE" : (String)Check.illegalstate.fail();
+		List<String> pNames = new LinkedList<String>();
+		StringBuilder uriTplParams = new StringBuilder("{");
+		String jsonBody = null;
+		for(VariableElement p: params) {
+			String jsonPath = null;
+			PathParam pp = p.getAnnotation(PathParam.class);
+			if(pp != null) {
+				jsonPath = pp.value();
+			} else {
+				QueryParam qp = p.getAnnotation(QueryParam.class);
+				if(qp != null) {
+					jsonPath = qp.value();
+				}
+			}
+			String clsType = null;//p.asType().getClass().getName();
+			TypeMirror tm = p.asType();
+			if(tm instanceof DeclaredType) {
+				clsType = ((DeclaredType)tm).asElement().getSimpleName().toString();
+			} else if(tm instanceof PrimitiveType) {
+				clsType = "PRIMITIVE";//((PrimitiveType)tm).asElement().getSimpleName().toString();
+			} else {
+				Check.illegalstate.fail();
+			}
+			String pname = p.getSimpleName().toString();
+			pNames.add(pname);
+			if(jsonPath != null) {
+				//TODO a.b.c
+				uriTplParams.append(jsonPath+":"+pname+",");
+			} else {
+				jsonBody = pname;
+			}
+		}
+		uriTplParams.append("}");
+		String sig = Strings.join(", ", pNames);
+		if(pNames.size() > 0) {
+			sig = sig + ", ";
+		}
+		String content = "";
+		if(jsonBody != null && ("POST".equals(method) || "PUT".equals(method))) {
+			content = ",contentType:'application/json;charset=UTF-8',data:JSON.stringify("+jsonBody+")";
+		}
+		w.write("\n"+member.getSimpleName().toString()+" : function("+sig+"okcb,errcb)  {" +
+			"var uri = uritpl('"+uriTpl.value()+"',"+uriTplParams.toString()+");" +
+			"var _errcb = errcb || dummy;" +
+			"jQuery.ajax({method:'"+method+"', url:uri, success:okcb, error:_errcb" + content +
+			"});\n},");
+	}
+
 
 	public void init(ProcessingEnvironment processingEnv) {
 		log("init");
