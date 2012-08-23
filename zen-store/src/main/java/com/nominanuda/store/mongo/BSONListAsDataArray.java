4ba0745c1699f92dfb0c44ff870b88fb2fa91415
michele <michelegonella@gmail.com>
2012-08-23 14:26:55 +0200

    basic mongo datastruct readonly wrappers

---
+++ zen-store/src/main/java/com/nominanuda/store/mongo/BSONListAsDataArray.java
@@ -0,0 +1,249 @@
+package com.nominanuda.store.mongo;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import com.nominanuda.dataobject.DataArray;
+import com.nominanuda.dataobject.DataObject;
+import com.nominanuda.dataobject.DataStruct;
+import com.nominanuda.dataobject.DataType;
+import com.nominanuda.lang.Check;
+
+import static com.nominanuda.store.mongo.BSONObjectAsDataObject.*;
+
+public class BSONListAsDataArray implements DataArray {
+	private final List<?> l;
+
+	public BSONListAsDataArray(List<?> o) {
+		this.l = o;
+	}
+
+	public List<Integer> getKeys() {
+		int len = getLength();
+		ArrayList<Integer> l = new ArrayList<Integer>(len);
+		for(int i = 0; i < len; i++) {
+			l.add(i);
+		}
+		return l;	
+	}
+
+	public Iterator<Integer> keyIterator() {
+		return getKeys().iterator();
+	}
+
+	public Object get(Integer key) {
+		return wrapIfNecessary(l.get(key));
+	}
+
+	public Object getStrict(Integer key) throws NullPointerException {
+		return Check.notNull(get(key));
+	}
+
+	public Object put(Integer key, Object o) {
+		return Check.unsupportedoperation.fail();
+	}
+
+	public Object remove(Integer key) {
+		return Check.unsupportedoperation.fail();
+	}
+
+	public boolean exists(Integer k) {
+		return get(k) != null;
+	}
+
+	public DataArray putNewArray(Integer key) {
+		return Check.unsupportedoperation.fail();
+	}
+
+	public Iterator<Object> iterator() {
+		final Iterator<?> itr = l.iterator();
+		return new Iterator<Object>() {
+			public void remove() {
+				Check.unsupportedoperation.fail();
+			}
+			public Object next() {
+				return wrapIfNecessary(itr.next());
+			}
+			public boolean hasNext() {
+				return itr.hasNext();
+			}
+		};
+	}
+
+	public DataObject putNewObject(Integer key) {
+		return Check.unsupportedoperation.fail();
+	}
+
+	public boolean isPrimitiveOrNull(Object o) {
+		Check.unsupportedoperation.fail();
+		return false;
+	}
+
+	public void setPathProperty(String path, Object value) {
+		Check.unsupportedoperation.fail();
+	}
+
+	public void setOrPushPathProperty(String path, Object value) {
+		Check.unsupportedoperation.fail();
+	}
+
+	public void setProperty(Object key, Object value) {
+		Check.unsupportedoperation.fail();
+	}
+
+	public void setOrPushProperty(Object key, Object value) {
+		Check.unsupportedoperation.fail();
+	}
+
+	public Object getPathSafe(String path) {
+		return BSONObjectAsDataObject.getPathSafe(this, path);
+	}
+
+	public String getString(Integer key) throws ClassCastException {
+		return (String)get(key);
+	}
+
+	public Number getNumber(Integer key) throws ClassCastException {
+		return (Number)get(key);
+	}
+
+	public Boolean getBoolean(Integer key) throws ClassCastException {
+		return (Boolean)get(key);
+	}
+
+	public DataObject getObject(Integer key) throws ClassCastException {
+		return (DataObject)get(key);
+	}
+
+	public DataArray getArray(Integer key) throws ClassCastException {
+		return (DataArray)get(key);
+	}
+
+	public String getPathSafeString(String path) throws ClassCastException {
+		return (String)getPathSafe(path);
+	}
+
+	public Number getPathSafeNumber(String path) throws ClassCastException {
+		return (Number)getPathSafe(path);
+	}
+
+	public Boolean getPathSafeBoolean(String path) throws ClassCastException {
+		return (Boolean)getPathSafe(path);
+	}
+
+	public DataObject getPathSafeObject(String path) throws ClassCastException {
+		return (DataObject)getPathSafe(path);
+	}
+
+	public DataArray getPathSafeArray(String path) throws ClassCastException {
+		return (DataArray)getPathSafe(path);
+	}
+
+	public Long getLong(Integer key) throws ClassCastException {
+		return (Long)get(key);
+	}
+
+	public Double getDouble(Integer key) throws ClassCastException {
+		return (Double)get(key);
+	}
+
+	public String getStrictString(Integer key) throws ClassCastException,
+			NullPointerException {
+		return (String)getStrict(key);
+	}
+
+	public Long getStrictLong(Integer key) throws ClassCastException,
+			NullPointerException {
+		return (Long)getStrict(key);
+	}
+
+	public Double getStrictDouble(Integer key) throws ClassCastException,
+			NullPointerException {
+		return (Double)getStrict(key);
+	}
+
+	public Boolean getStrictBoolean(Integer key) throws ClassCastException,
+			NullPointerException {
+		return (Boolean)getStrict(key);
+	}
+
+	public DataObject getStrictObject(Integer key) throws ClassCastException,
+			NullPointerException {
+		return (DataObject)getStrict(key);
+	}
+
+	public DataArray getStrictArray(Integer key) throws ClassCastException,
+			NullPointerException {
+		return (DataArray)getStrict(key);
+	}
+
+	public Long putLong(Integer key, Long o) {
+		return Check.unsupportedoperation.fail();
+	}
+
+	public Double putDouble(Integer key, Double o) {
+		return Check.unsupportedoperation.fail();
+	}
+
+	public String putString(Integer key, String o) {
+		return Check.unsupportedoperation.fail();
+	}
+
+	public Boolean putBoolean(Integer key, Boolean o) {
+		return Check.unsupportedoperation.fail();
+	}
+
+	public DataObject putObject(Integer key, DataObject o) {
+		return Check.unsupportedoperation.fail();
+	}
+
+	public DataArray putArray(Integer key, DataArray o) {
+		return Check.unsupportedoperation.fail();
+	}
+
+	public DataStruct getParent() {
+		return Check.unsupportedoperation.fail();
+	}
+
+	public DataStruct getRoot() {
+		return Check.unsupportedoperation.fail();
+	}
+
+	public boolean isArray() {
+		return true;
+	}
+
+	public boolean isObject() {
+		return false;
+	}
+
+	public String getType() {
+		return DataType.array.name();
+	}
+
+	public DataArray asArray() throws ClassCastException {
+		return this;
+	}
+
+	public DataObject asObject() throws ClassCastException {
+		throw new ClassCastException();
+	}
+
+	public Object add(Object val) {
+		return Check.unsupportedoperation.fail();
+	}
+
+	public DataObject addNewObject() {
+		return Check.unsupportedoperation.fail();
+	}
+
+	public DataArray addNewArray() {
+		return Check.unsupportedoperation.fail();
+	}
+
+	public int getLength() {
+		return l.size();
+	}
+
+}
---
+++ zen-store/src/main/java/com/nominanuda/store/mongo/BSONObjectAsDataObject.java
@@ -0,0 +1,253 @@
+package com.nominanuda.store.mongo;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.bson.BSONObject;
+
+import com.nominanuda.dataobject.DataArray;
+import com.nominanuda.dataobject.DataObject;
+import com.nominanuda.dataobject.DataStruct;
+import com.nominanuda.dataobject.DataType;
+import com.nominanuda.dataobject.PropertyBag;
+import com.nominanuda.lang.Check;
+import com.nominanuda.lang.Maths;
+
+import static com.nominanuda.dataobject.DataStructHelper.STRUCT;
+
+public class BSONObjectAsDataObject implements DataObject {
+	private final BSONObject bson;
+
+	public BSONObjectAsDataObject(BSONObject bson) {
+		this.bson = bson;
+	}
+
+	public Iterator<String> keyIterator() {
+		return bson.keySet().iterator();
+	}
+
+	public Object get(String key) {
+		Object o = bson.get(key);
+		return wrapIfNecessary(o);
+	}
+
+	static Object wrapIfNecessary(Object o) {
+		if(STRUCT.isPrimitiveOrNull(o)) {
+			return o;
+		} else if(o instanceof List) {
+			return new BSONListAsDataArray((List<?>)o);
+		} else {
+			return new BSONObjectAsDataObject(
+				Check.illegalstate.assertInstanceOf(o, BSONObject.class));
+		}
+	}
+
+	public boolean exists(String k) {
+		return bson.containsField(k);
+	}
+
+	public List<String> getKeys() {
+		return new LinkedList<String>(bson.keySet());
+	}
+
+	public Object getPathSafe(String path) {
+		return getPathSafe(this, path);
+	}
+	static Object getPathSafe(Object o, String path) {
+		PropertyBag<Object> _target = asObjectKeyedDataStruct(o);
+		String[] pathBits = path.split("\\.");
+		int len = pathBits.length;
+		for (int i = 0; i < len; i++) {
+			String pathBit = pathBits[i];
+			Object k = toStringOrIntKey(pathBit);
+			Object newTarget = _target.get(k);
+			if (STRUCT.isPrimitiveOrNull(newTarget)) {
+				return i == len - 1 ? newTarget : null;
+			} else {
+				_target = (PropertyBag<Object>) newTarget;
+			}
+		}
+		return _target;
+	}
+	@SuppressWarnings("unchecked")
+	static PropertyBag<Object> asObjectKeyedDataStruct(Object o) {
+		return (PropertyBag)o;
+	}
+	static Object toStringOrIntKey(String s) {
+		Check.notNull(s);
+		return Maths.isInteger(s) ? Integer.valueOf(s) : s;
+	}
+	public Object put(String key, Object o) {
+		return Check.unsupportedoperation.fail();
+	}
+	public DataStruct getParent() {
+		return Check.unsupportedoperation.fail();
+	}
+
+	public DataStruct getRoot() {
+		return Check.unsupportedoperation.fail();
+	}
+
+	public boolean isArray() {
+		return false;
+	}
+
+	public boolean isObject() {
+		return true;
+	}
+
+	public String getType() {
+		return DataType.object.name();
+	}
+
+	public DataArray asArray() throws ClassCastException {
+		throw new ClassCastException();
+	}
+
+	public DataObject asObject() throws ClassCastException {
+		return this;
+	}
+
+	public Object getStrict(String key) throws NullPointerException {
+		return Check.notNull(get(key));
+	}
+
+	public Object remove(String key) {
+		return Check.unsupportedoperation.fail();
+	}
+
+	public DataArray putNewArray(String key) {
+		return Check.unsupportedoperation.fail();
+	}
+
+	public DataObject putNewObject(String key) {
+		return Check.unsupportedoperation.fail();
+	}
+
+	public boolean isPrimitiveOrNull(Object o) {
+		Check.unsupportedoperation.fail();
+		return false;
+	}
+
+	public void setPathProperty(String path, Object value) {
+		Check.unsupportedoperation.fail();
+	}
+
+	public void setOrPushPathProperty(String path, Object value) {
+		Check.unsupportedoperation.fail();
+	}
+
+	public void setProperty(Object key, Object value) {
+		Check.unsupportedoperation.fail();
+	}
+
+	public void setOrPushProperty(Object key, Object value) {
+		Check.unsupportedoperation.fail();
+	}
+
+	public String getString(String key) throws ClassCastException {
+		return (String)get(key);
+	}
+
+	public Number getNumber(String key) throws ClassCastException {
+		return (Number)get(key);
+	}
+
+	public Boolean getBoolean(String key) throws ClassCastException {
+		return (Boolean)get(key);
+	}
+
+	public DataObject getObject(String key) throws ClassCastException {
+		return (DataObject)get(key);
+	}
+
+	public DataArray getArray(String key) throws ClassCastException {
+		return (DataArray)get(key);
+	}
+
+	public String getPathSafeString(String path) throws ClassCastException {
+		return (String)getPathSafe(path);
+	}
+
+	public Number getPathSafeNumber(String path) throws ClassCastException {
+		return (Number)getPathSafe(path);
+	}
+
+	public Boolean getPathSafeBoolean(String path) throws ClassCastException {
+		return (Boolean)getPathSafe(path);
+	}
+
+	public DataObject getPathSafeObject(String path) throws ClassCastException {
+		return (DataObject)getPathSafe(path);
+	}
+
+	public DataArray getPathSafeArray(String path) throws ClassCastException {
+		return (DataArray)getPathSafe(path);	}
+
+	public Long getLong(String key) throws ClassCastException {
+		return (Long)get(key);
+	}
+
+	public Double getDouble(String key) throws ClassCastException {
+		return (Double)get(key);
+	}
+
+	public String getStrictString(String key) throws ClassCastException,
+			NullPointerException {
+		return Check.notNull(getString(key));
+	}
+
+	public Long getStrictLong(String key) throws ClassCastException,
+			NullPointerException {
+		return Check.notNull(getLong(key));
+	}
+
+	public Double getStrictDouble(String key) throws ClassCastException,
+			NullPointerException {
+		return Check.notNull(getDouble(key));
+	}
+
+	public Boolean getStrictBoolean(String key) throws ClassCastException,
+			NullPointerException {
+		return Check.notNull(getBoolean(key));
+	}
+
+	public DataObject getStrictObject(String key) throws ClassCastException,
+			NullPointerException {
+		return Check.notNull(getObject(key));
+	}
+
+	public DataArray getStrictArray(String key) throws ClassCastException,
+			NullPointerException {
+		return Check.notNull(getArray(key));
+	}
+
+	public Long putLong(String key, Long o) {
+		return Check.unsupportedoperation.fail();
+	}
+
+	public Double putDouble(String key, Double o) {
+		return Check.unsupportedoperation.fail();
+	}
+
+	public String putString(String key, String o) {
+		return Check.unsupportedoperation.fail();
+	}
+
+	public Boolean putBoolean(String key, Boolean o) {
+		return Check.unsupportedoperation.fail();
+	}
+
+	public DataObject putObject(String key, DataObject o) {
+		return Check.unsupportedoperation.fail();
+	}
+
+	public DataArray putArray(String key, DataArray o) {
+		return Check.unsupportedoperation.fail();
+	}
+
+	public DataObject with(String k, Object v) {
+		return Check.unsupportedoperation.fail();
+	}
+}
