57ddd78370dfcd278e9e0d883f4309bea53c09d9
michele <michelegonella@gmail.com>
2012-09-11 23:43:26 +0200

    added license header

--- zen-dataobject/src/test/java/com/nominanuda/dataobject/schema/JclValidatorTest.java
+++ zen-dataobject/src/test/java/com/nominanuda/dataobject/schema/JclValidatorTest.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2008-2011 the original author or authors.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.nominanuda.dataobject.schema;
 
 import static org.junit.Assert.*;
@@ -15,18 +30,6 @@ import com.nominanuda.lang.InstanceFactory;
 
 public class JclValidatorTest {
 
-	//@Test
-	public void test() throws Exception {
-		JclValidator jclValidator = new JclValidator("{a:{r,c,e}}");
-		DataObjectImpl in = new DataObjectImpl();
-		in.put("a", 1);
-		JsonPipeline p = new JsonPipeline()
-			.add(new InstanceFactory<JsonTransformer>(jclValidator))
-			.withLooseParser()
-			.complete();
-		DataStruct res = p.build(new StringReader("{a:{b:false,c:1.1,d:1.0,e:[1,[1],{x:1}]}}")).apply();
-		System.err.println(res.toString());
-	}
 
 	@Test
 	public void test1() throws Exception {
