0b8434881e7561f162caaa30cac45f0a3d1fb49f
michele gonella <michelegonella@gmail.com>
2014-07-28 21:26:09 +0200

    double charset normalized

--- zen-webservice/src/test/java/com/nominanuda/hyperapi/HyperApiWsSkeltonTest.java
+++ zen-webservice/src/test/java/com/nominanuda/hyperapi/HyperApiWsSkeltonTest.java
@@ -47,7 +47,7 @@ public class HyperApiWsSkeltonTest {
 		DataObject foo = new DataObjectImpl();
 		foo.put("foo", "FOO");
 		request.setEntity(new StringEntity(new DataStructHelper().toJsonString(foo),
-			ContentType.create(HttpProtocol.CT_APPLICATION_JSON_CS_UTF8, HttpProtocol.CS_UTF_8)));
+			ContentType.create(HttpProtocol.CT_APPLICATION_JSON, HttpProtocol.CS_UTF_8)));
 		HttpResponse response = skelton.handle(request);
 		DataStruct result = new JSONParser().parse(new InputStreamReader(response.getEntity().getContent()));
 		Assert.assertEquals("FOO", ((DataObject)result).get("foo"));
