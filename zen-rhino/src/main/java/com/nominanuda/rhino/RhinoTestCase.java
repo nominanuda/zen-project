cbcc54902569fa52e896f1c1176d2fa0c95e95ed
azum <azumian@gmail.com>
2016-01-09 14:47:26 +0100

    more configurable rhino test cases

--- zen-rhino/src/main/java/com/nominanuda/rhino/RhinoTestCase.java
+++ zen-rhino/src/main/java/com/nominanuda/rhino/RhinoTestCase.java
@@ -15,8 +15,11 @@
  */
 package com.nominanuda.rhino;
 
+import java.io.File;
+import java.io.FilenameFilter;
 import java.net.URL;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.LinkedList;
 import java.util.List;
@@ -26,14 +29,19 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.mozilla.javascript.Context;
+import org.mozilla.javascript.DebuggableRhinoEmbedding;
+import org.mozilla.javascript.MethodArgCoercer;
 import org.mozilla.javascript.PluggableMethodArgCoercer;
 import org.mozilla.javascript.PluggableWrapFactory;
 import org.mozilla.javascript.RhinoEmbedding;
 import org.mozilla.javascript.RhinoHelper;
 import org.mozilla.javascript.ScriptableObject;
+import org.mozilla.javascript.WrapFactory;
 import org.mozilla.javascript.WrappedException;
 
 import com.nominanuda.code.Nullable;
+import com.nominanuda.dataobject.DataArray;
+import com.nominanuda.dataobject.DataObject;
 import com.nominanuda.dataobject.DataStruct;
 import com.nominanuda.lang.Collections;
 import com.nominanuda.lang.ObjectConvertor;
@@ -52,65 +60,49 @@ public abstract class RhinoTestCase {
 	protected RhinoEmbedding rhinoEmbedding;
 	protected ScriptableObject testScope;
 	protected Context testContext;
+	
 	@Before
 	public void setUp() throws Exception {
 		new PluggableURLStreamHandlerFactory().installToJvm();
-		rhinoEmbedding = new RhinoEmbedding();
-		rhinoEmbedding.setDebug(isDebug());
-		@SuppressWarnings("unchecked")
-		Map<Class<?>, Tuple2<ObjectConvertor<Object, Object, Exception>,Integer>> coerceTypeMap = 
-			Collections.buildMap(LinkedHashMap.class, DataStruct.class, new Tuple2<ObjectConvertor<?, ?, ?>,Integer>(
-					new ToDataStructCoercer(), 1));
-		PluggableWrapFactory wf = new PluggableWrapFactory(rhinoEmbedding);
-		wf.setConvertors(Arrays.asList(new DataStructConvertor()));
-		rhinoEmbedding.setWrapFactory(wf);
-		PluggableMethodArgCoercer coercer = new PluggableMethodArgCoercer();
-		coercer.setConvertors(coerceTypeMap);
-		rhinoEmbedding.setMethodArgCoercer(coercer);
+		(rhinoEmbedding = new DebuggableRhinoEmbedding()).setDebug(isDebug());
+		rhinoEmbedding.setWrapFactory(getWrapFactory());
+		rhinoEmbedding.setMethodArgCoercer(getMethodArgCoercer());
 		rhinoEmbedding.init();
 		testContext = rhinoEmbedding.enterContext();
-		testScope = rhino.protocloneScriptable(testContext, 
-				rhino.createTopScope(testContext, true));
+		testScope = rhino.protocloneScriptable(testContext, rhino.createTopScope(testContext, true));
 		ModuleRegistry moduleRegistry = new ModuleRegistry();
-		List<? extends ModuleFactory> l = buildModuleFactories();
-		moduleRegistry.setModuleFactories(l);
+		moduleRegistry.setModuleFactories(buildModuleFactories());
 		Require require = new Require();
 		require.setRegistry(moduleRegistry);
 		rhino.putProperty(testScope, "require", require);
-		//URL url = new URL("classpath:/com/nominanuda/rhino/JUnit.js");
+		//URL url = new URL("classpath:com/nominanuda/rhino/JUnit.js");
 		//rhino.evaluateURL(testContext, url, testScope);
 		onSetup();
 	}
 
-	protected List<? extends ModuleFactory> buildModuleFactories() {
-		List<ModuleFactory> l = new LinkedList<ModuleFactory>();
-		l.add(new SourceModuleFactory());
-		HostObjectFactory mf = new HostObjectFactory();
-		mf.addObject("console", "com.nominanuda.rhino.host.Console");
-		l.add(mf);
-		Map<String, Object> m = java.util.Collections.emptyMap();
-		JavaObjectFactory jof = new JavaObjectFactory(m);
-		l.add(jof);
-		return l;
-	}
 	protected void onSetup() throws Exception {
 	}
+	
 	@After
 	public void tearDown() throws Exception {
 		Context.exit();
 	}
+	
 	protected Object runJSTestCase(URL url) throws Exception {
 		try {
-			Object res = rhino.evaluateURL(testContext, url, testScope);
-			return res;
+			return rhino.evaluateURL(testContext, url, testScope);
 		} catch (Exception e) {
-			if(e instanceof WrappedException) {
+			if (e instanceof WrappedException) {
 				WrappedException ee = (WrappedException)e;
 				//TODO
-				String s = ee.getScriptStackTrace();
+				String s = ee.getScriptStackTrace(new FilenameFilter() {
+					public boolean accept(File dir, String name) {
+						return true;
+					}
+				});
 				System.err.println(s);
 				Throwable t = ee.getWrappedException();
-				if(t instanceof Error) {
+				if (t instanceof Error) {
 					throw (Error)t;
 				}
 				throw (Exception)t;
@@ -118,25 +110,58 @@ public abstract class RhinoTestCase {
 			throw e;
 		}
 	}
+	
 	@Test
 	public void testRunAll() throws Exception {
 		String[] scriptLocations = getScriptUrls();
-		if(scriptLocations == null) {
+		if (scriptLocations == null) {
 			scriptLocations = new String[] {
-				"classpath:/"
-				+getClass().getPackage().getName().replace('.', '/')
-				+"/"+getClass().getSimpleName()+".js"
+				"classpath:" + getClass().getPackage().getName().replace('.', '/')
+				+ "/" + getClass().getSimpleName() + ".js"
 			};
 		}
-		for(String location : scriptLocations) {
+		for (String location : scriptLocations) {
 			runJSTestCase(new URL(location));
 		}
+	}
+	
+	
+	/* setup */
+
+	protected boolean isDebug() {
+		return false;
+	}
+	
+	protected WrapFactory getWrapFactory() {
+		PluggableWrapFactory wf = new PluggableWrapFactory(rhinoEmbedding);
+		wf.setConvertors(Arrays.asList(new DataStructConvertor()));
+		return wf;
+	}
+	
+	protected MethodArgCoercer getMethodArgCoercer() {
+		return new PluggableMethodArgCoercer(Collections.buildMap(LinkedHashMap.class,
+			String.class, new Tuple2<ObjectConvertor<?, ?, ?>, Integer>(new UndefinedCoercer(), 1),
+			DataArray.class, new Tuple2<ObjectConvertor<?, ?, ?>, Integer>(new ToDataArrayCoercer(), 1),
+			DataObject.class, new Tuple2<ObjectConvertor<?, ?, ?>, Integer>(new ToDataObjectCoercer(), 1),
+			DataStruct.class, new Tuple2<ObjectConvertor<?, ?, ?>, Integer>(new ToDataStructCoercer(), 1),
+			Object.class, new Tuple2<ObjectConvertor<?, ?, ?>, Integer>(new ToDataStructCoercer(), 2)));
+	}
+	
+	protected List<? extends ModuleFactory> buildModuleFactories() {
+		List<ModuleFactory> l = new LinkedList<ModuleFactory>();
+		l.add(new SourceModuleFactory());
+		HostObjectFactory mf = new HostObjectFactory();
+		mf.addObject("console", "com.nominanuda.rhino.host.Console");
+		l.add(mf);
+		l.add(new JavaObjectFactory(buildJavaObjectsMap()));
+		return l;
+	}
+	
+	protected Map<String, Object> buildJavaObjectsMap() {
+		return new HashMap<String, Object>();
 	}
 
 	protected @Nullable String[] getScriptUrls() {
 		return null;
-	}
-	protected boolean isDebug() {
-		return false;
 	}
 }
