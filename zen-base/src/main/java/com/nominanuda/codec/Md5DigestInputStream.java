11b5e3acd69dfbb4b7eb6c32d24a2eb15fcb05b5
azum <azumian@gmail.com>
2016-06-15 20:16:21 +0200

    md5 sha1 input streams

---
+++ zen-base/src/main/java/com/nominanuda/codec/Md5DigestInputStream.java
@@ -0,0 +1,16 @@
+package com.nominanuda.codec;
+
+import java.io.InputStream;
+import java.security.DigestInputStream;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class Md5DigestInputStream extends DigestInputStream {
+	public Md5DigestInputStream(InputStream stream) throws NoSuchAlgorithmException {
+		super(stream, (MessageDigest.getInstance("MD5")));
+	}
+
+	public String hex() {
+		return Hex.encode(getMessageDigest().digest());
+	}
+}
---
+++ zen-base/src/main/java/com/nominanuda/codec/Sha1DigestInputStream.java
@@ -0,0 +1,17 @@
+package com.nominanuda.codec;
+
+import java.io.InputStream;
+import java.security.DigestInputStream;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class Sha1DigestInputStream extends DigestInputStream {
+
+	public Sha1DigestInputStream(InputStream stream) throws NoSuchAlgorithmException {
+		super(stream, MessageDigest.getInstance("SHA1"));
+	}
+	
+	public String hex() {
+		return Hex.encode(getMessageDigest().digest());
+	}
+}
