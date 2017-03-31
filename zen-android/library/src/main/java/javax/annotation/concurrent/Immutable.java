d0a7290a5a8e7a7a746158bd250591b9d4007f96
azum <azumian@gmail.com>
2017-03-31 19:29:56 +0200

    annotations deps fixing for android

--- zen-android/library/src/main/java/javax/annotation/concurrent/Immutable.java
+++
@@ -1,7 +0,0 @@
-package javax.annotation.concurrent;
-
-@java.lang.annotation.Target(value={java.lang.annotation.ElementType.TYPE})
-@java.lang.annotation.Retention(value=java.lang.annotation.RetentionPolicy.RUNTIME)
-public @interface Immutable {
-
-}
