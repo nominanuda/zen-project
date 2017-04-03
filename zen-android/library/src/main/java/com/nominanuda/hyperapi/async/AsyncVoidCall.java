cfc442f5f6e5e8814c64595b75d66d3babdf905a
azum <azumian@gmail.com>
2017-04-03 14:41:36 +0200

    fix

--- zen-android/library/src/main/java/com/nominanuda/hyperapi/async/AsyncVoidCall.java
+++ zen-android/library/src/main/java/com/nominanuda/hyperapi/async/AsyncVoidCall.java
@@ -2,14 +2,12 @@ package com.nominanuda.hyperapi.async;
 
 import android.app.Activity;
 
-import java.util.concurrent.Callable;
-
 /**
  * Created by azum on 27/03/17.
  */
 
 public class AsyncVoidCall<API> extends AsyncCall<API, Boolean> {
-	public AsyncVoidCall(Activity activity, API api, Callback<API> callFnc, Callback<Boolean> resultFnc, Callable<Exception> errorFnc) {
+	public AsyncVoidCall(Activity activity, API api, Callback<API> callFnc, Callback<Boolean> resultFnc, Callback<Exception> errorFnc) {
 		super(activity, api, apiProxy -> {
 			callFnc.apply(apiProxy);
 			return null;
